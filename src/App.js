1f4e193b87e84771c9e13b35de5fa9fafff22e42
candeaquiles <80926300+candeaquiles@users.noreply.github.com>
2021-10-19 20:41:56 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Main from './components/Main/NavBar/Main/Main';
+
+
+
+ 
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Main/>
+    </div>
+   
+  );
+}
+
+
+
+export default App;

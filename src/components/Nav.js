033f79750c059fbcd05b932d6d726c1d9a42712b
candeaquiles <80926300+candeaquiles@users.noreply.github.com>
2021-10-19 20:41:21 -0300

    Add files via upload

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+       <nav>
+           <h1>Undefined Web Site</h1>
+           <ul>
+               <li>Home</li>
+               <li>Products</li>
+               <li>About Us</li>
+               <li>Contact Us</li>
+           </ul>
+       </nav>
+    )
+}
+
+export default Nav

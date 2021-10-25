0c287998ad6350e272c0bea4cc90ceddb8b1c7b0
CandeAquiles <cande9449@gmail.com>
2021-10-25 17:16:31 -0300

    Finish

---
+++ Documents/myfirstapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+     <p>Hi wordi</p>
+    </div>
+  );
+}
+
+export default App;
